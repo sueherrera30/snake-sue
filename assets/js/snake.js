bedf69947513d460057cfb1356434627d1b5af15
sue herrera <sueherrera30@gmail.com>
2017-04-07 01:37:16 -0500

    version final basica

--- assets/js/snake.js
+++ assets/js/snake.js
@@ -10,21 +10,36 @@ document.addEventListener("keydown",avanzar);
      case 40:
       posicionY= posicionY + 20;
       viborita.style.marginTop = posicionY + 'px';
+      gameOver();
       break;
       //up
       case 38:
       posicionY= posicionY - 20;
       viborita.style.marginTop = posicionY  + 'px';
+      gameOver();
       break;
       //right
       case 39:
        posicionX = posicionX + 20;
        viborita.style.marginLeft = posicionX + 'px';
+       gameOver();
        break;
        //left
        case 37:
        posicionX = posicionX - 20;
        viborita.style.marginLeft = posicionX  + 'px';
+       gameOver();
        break;
    }
  }
+function gameOver(){
+  if(posicionX > 480||posicionX< 0){
+alert("gameover!");
+ window.location.reload();
+  }
+  if(posicionY > 480 ||posicionY < 0) {
+  alert("gameover!");
+  window.location.reload();
+  }
+
+}
